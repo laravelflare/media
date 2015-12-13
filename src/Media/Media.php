386df08d1919a07a4c9caf269c8c3ae9296d8432
Aden Fraser <aden@jacobbailey.com>
2015-12-13 22:34:02 +0000

    Ability to link to Media and request a defined size

--- src/Media/Media.php
+++ src/Media/Media.php
@@ -31,7 +31,39 @@ class Media extends Model
      *
      * @var array
      */
-    protected $appends = ['human_size'];
+    protected $appends = ['human_size', 'link'];
+
+    /**
+     * Returns the media link in the requested size
+     * 
+     * @return 
+     */
+    public function size($width = null, $height = null)
+    {
+        foreach (\Config::get('flare-config.media.resize') as $sizeArray) {
+
+            if ($width && $height && ($width == $sizeArray[0]) && ($height == $sizeArray[1])) {
+                return $this->path ? url('uploads/media/'.$sizeArray[0].'-'.(array_key_exists(1, $sizeArray) ? $sizeArray[1] : $sizeArray[0]).'-'.$this->path) : null;
+            }
+
+            if ($width && ($width == $sizeArray[0])) {
+                return $this->path ? url('uploads/media/'.$sizeArray[0].'-'.(array_key_exists(1, $sizeArray) ? $sizeArray[1] : $sizeArray[0]).'-'.$this->path) : null;
+            }
+
+        }
+
+        return $this->link;
+    }
+
+    /**
+     * Return the Full Media Link
+     * 
+     * @return string
+     */
+    public function getLinkAttribute()
+    {
+        return $this->path ? url('uploads/media/'.$this->path) : null;
+    }
 
     /**
      * Convert Media Size to a Human Readable Format
