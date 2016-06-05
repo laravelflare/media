380a9d3f7c2fc370c2cbd34a77ced87675fc8491
Aden Fraser <aden.fraser@bc-legal.co.uk>
2016-06-05 21:28:36 +0100

    Revert "Stop publishing Media Views in Media Service Provider"
    This reverts commit d3e2ee24ccd8e4e32c2e9d0dc0eecf8834a6381e.

--- src/Media/MediaServiceProvider.php
+++ src/Media/MediaServiceProvider.php
@@ -27,6 +27,12 @@ class MediaServiceProvider extends ServiceProvider
             __DIR__.'/Database/Migrations' => base_path('database/migrations'),
         ]);
 
+        // Views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'flare');
+        $this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/flare'),
+        ]);
+
         $this->registerBladeOperators();
     }
 
